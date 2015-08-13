1c8633ba0ec8d14e5821af5bb39ca09c9f895126
GRappolt <grappolt@rcn.com>
2015-08-13 12:20:51 -0400

    Get text display for the Instruction activity to work as needed.

--- app/src/main/java/com/roachcitysoftware/goldenkey/InstructionsActivityFragment.java
+++ app/src/main/java/com/roachcitysoftware/goldenkey/InstructionsActivityFragment.java
@@ -5,11 +5,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.text.Html;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class InstructionsActivityFragment extends Fragment {
+
+    private final String instructionsText = "<body><H1>Background</H1>" +
+            "<P>This world can be both heaven and hell.</P></body>";
 
     public InstructionsActivityFragment() {
     }
@@ -17,6 +22,9 @@ public class InstructionsActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_instructions, container, false);
+        View v = inflater.inflate(R.layout.fragment_instructions, container, false);
+        TextView instructionsView = (TextView) v.findViewById(R.id.textView3);
+        instructionsView.setText(Html.fromHtml(instructionsText, null, null));
+        return v;
     }
 }

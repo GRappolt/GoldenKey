88b4ad899189c9c2dd2e1c1c6bdeaf741a40dc5f
GRappolt <grappolt@rcn.com>
2015-09-09 16:22:19 -0400

    Initial, partial implementation of BlessingProvider (not yet a working class).

--- app/src/main/java/com/roachcitysoftware/goldenkey/GrandContract.java
+++ app/src/main/java/com/roachcitysoftware/goldenkey/GrandContract.java
@@ -19,4 +19,15 @@ public class GrandContract {
         public static final String ID = BaseColumns._ID;
         public static final String BLESSING = "blessing";
     }
+
+    // BlessingProvider specific constants
+    public static final String AUTHORITY_1 = "com.roachcitysoftware.goldenkey.BlessingProvider";
+    public static final String CONTENT_URI_1 = "content://" + AUTHORITY_1 + "/" + TABLE_1;
+    public static final int BLESSING_ITEM = 1;
+    public static final int BLESSING_DIR = 2;
+    public static final String BLESSING_TYPE_ITEM =
+            "vnd.android.cursor.item/vnd.com.roachcitysoftware.goldenkey.provider.blessing";
+    public static final String BLESSING_TYPE_DIR =
+            "vnd.android.cursor.dir/vnd.com.roachcitysoftware.goldenkey.provider.blessing";
 }
+

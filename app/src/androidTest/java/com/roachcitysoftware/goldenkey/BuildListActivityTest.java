0acf30d0b174e8f36d5bb428d1d2771eea8c0615
GRappolt <grappolt@rcn.com>
2015-11-12 12:25:49 -0500

    Added basic tests (for initial state) of BuildListActivity.

---
+++ app/src/androidTest/java/com/roachcitysoftware/goldenkey/BuildListActivityTest.java
@@ -0,0 +1,99 @@
+package com.roachcitysoftware.goldenkey;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.ViewAsserts;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Unit tests for BuildListActivity by GeorgeR created 11/12/2015.
+ */
+public class BuildListActivityTest extends ActivityInstrumentationTestCase2<BuildListActivity>
+{
+    private BuildListActivity mBuildListActivity;
+    private TextView mNewListItems;
+    private TextView mHintItems;
+    private Button mAddButton;
+    private Button mHintsButon;
+
+    public BuildListActivityTest () { super(BuildListActivity.class);}
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mBuildListActivity = getActivity();
+        mNewListItems = (TextView) mBuildListActivity.findViewById(R.id.new_list_items);
+        mHintItems = (TextView) mBuildListActivity.findViewById(R.id.hint_items);
+        mAddButton = (Button) mBuildListActivity.findViewById(R.id.add_button);
+        mHintsButon = (Button) mBuildListActivity.findViewById(R.id.hint_button);
+    }
+
+    public void testPreconditions() {
+        assertNotNull("mBuildListActivity is null.", mBuildListActivity);
+        assertNotNull("mNewListItems is null.", mNewListItems);
+        assertNotNull("mHintItems is null.", mHintItems);
+        assertNotNull("mAddButton is null.", mAddButton);
+        assertNotNull("mHintsButon is null.", mHintsButon);
+    }
+
+    public void testBuildListActivityAddButton_text ()
+    {
+        final String expected = mBuildListActivity.getString(R.string.add_button);
+        final String actual = mAddButton.getText().toString();
+        assertEquals(expected, actual);
+    }
+
+    public void testBuildListActivityAddButton_layout ()
+    {
+        final View decorView = mBuildListActivity.getWindow().getDecorView();
+
+        ViewAsserts.assertOnScreen(decorView, mAddButton);
+
+        final ViewGroup.LayoutParams layoutParams = mAddButton.getLayoutParams();
+        assertNotNull(layoutParams);
+        assertEquals(layoutParams.width, WindowManager.LayoutParams.WRAP_CONTENT);
+        assertEquals(layoutParams.height, WindowManager.LayoutParams.WRAP_CONTENT);
+    }
+
+    public void testBuildListActivityHintsButton_text ()
+    {
+        final String expected = mBuildListActivity.getString(R.string.hint_button_1);
+        final String actual = mHintsButon.getText().toString();
+        assertEquals(expected, actual);
+    }
+
+    public void testBuildListActivityHintsButton_layout ()
+    {
+        final View decorView = mBuildListActivity.getWindow().getDecorView();
+
+        ViewAsserts.assertOnScreen(decorView, mHintsButon);
+
+        final ViewGroup.LayoutParams layoutParams = mHintsButon.getLayoutParams();
+        assertNotNull(layoutParams);
+        assertEquals(layoutParams.width, WindowManager.LayoutParams.WRAP_CONTENT);
+        assertEquals(layoutParams.height, WindowManager.LayoutParams.WRAP_CONTENT);
+    }
+
+    public void testBuildListActivityNewListItems_layout ()
+    {
+        final View decorView = mBuildListActivity.getWindow().getDecorView();
+
+        ViewAsserts.assertOnScreen(decorView, mNewListItems);
+
+        final ViewGroup.LayoutParams layoutParams = mNewListItems.getLayoutParams();
+        assertNotNull(layoutParams);
+        assertEquals(layoutParams.width, WindowManager.LayoutParams.MATCH_PARENT);
+        assertEquals(layoutParams.height, WindowManager.LayoutParams.WRAP_CONTENT);
+    }
+
+    public void testBuildListActivityHintItems_layout ()
+    {
+        final View decorView = mBuildListActivity.getWindow().getDecorView();
+
+        ViewAsserts.assertOnScreen(decorView, mHintItems);
+        assertTrue(View.INVISIBLE == mHintItems.getVisibility());
+    }
+}

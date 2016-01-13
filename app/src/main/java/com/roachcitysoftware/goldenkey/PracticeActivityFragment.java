fca76f763908b8ac384e3eaacc2d305f6c87c523
GRappolt <grappolt@rcn.com>
2016-01-13 11:22:13 -0500

    Add infrastructure for the Practice activity.

--- app/src/main/java/com/roachcitysoftware/goldenkey/PracticeActivityFragment.java
+++ app/src/main/java/com/roachcitysoftware/goldenkey/PracticeActivityFragment.java
@@ -1,11 +1,15 @@
 package com.roachcitysoftware.goldenkey;
 
 
+import android.content.ContentProviderClient;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -13,6 +17,16 @@ import android.view.ViewGroup;
  */
 public class PracticeActivityFragment extends Fragment {
 
+    private static final String TAG = PracticeActivityFragment.class.getSimpleName();
+    private EditText mBlessing;
+    private Button mNextButton;
+    private class BlessingEntry {
+        public long rowID;
+        public String blessingText;
+        public boolean changed;
+    }
+    private BlessingEntry [] mBlessingList;
+    private int mCurrentBlessing;
 
     public PracticeActivityFragment() {
         // Required empty public constructor
@@ -23,7 +37,30 @@ public class PracticeActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_practice_activity, container, false);
+        View v = inflater.inflate(R.layout.fragment_practice, container, false);
+        mBlessing = (EditText) v.findViewById(R.id.blessing_items);
+        mNextButton = (Button) v.findViewById(R.id.next_button);
+        LoadBlessingList(v);
+        Log.d(TAG, "onCreateView");
+        return v;
     }
 
+    private void LoadBlessingList (View v)
+    {
+        ContentProviderClient cpc =
+                v.getContext().getContentResolver().acquireContentProviderClient(GrandContract.CONTENT_URI_1);
+        if (cpc == null) {
+            Log.d(TAG, "LoadBlessingList failed - can't get Content Resolver");
+            return;
+        }
+        BlessingProvider bp = (BlessingProvider) cpc.getLocalContentProvider();
+        if (bp == null) {
+            Log.d(TAG, "LoadBlessingList failed - can't get BlessingProvider");
+            return;
+        }
+        // Do the real work here
+
+        // Clean up
+        cpc.release();
+    }
 }
